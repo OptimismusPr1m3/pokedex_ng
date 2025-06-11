a4a83bab17e8f88dcf82e11f54433775511bb46c
Sebastian Wolff <bastiwolff432@gmail.com>
2025-06-11 16:45:54 +0200

    added landingpage component with its headbar

---
+++ src/app/landingpage/landingpage.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-landingpage',
+  imports: [MatIconModule],
+  templateUrl: './landingpage.component.html',
+  styleUrl: './landingpage.component.scss'
+})
+
+export class LandingpageComponent {
+
+}
