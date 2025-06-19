69b352b3b378f4c88753b6266b0ba6d97a313178
Sebastian Wolff <bastiwolff432@gmail.com>
2025-06-19 14:12:35 +0200

    added card styling, background is now chosen by type of the pokemon

--- src/app/cards/smallcard/smallcard.component.ts
+++ src/app/cards/smallcard/smallcard.component.ts
@@ -11,6 +11,27 @@ import { CommonModule } from '@angular/common';
 export class SmallcardComponent {
   pokemon = input<Pokemon>();
 
+  BACKGROUND_COLORS: { [key: string]: string } = {
+    fire: '#ff5733', // Lebendiges Korallenrot für Feuer
+    water: '#00aaff', // Frisches Türkisblau für Wasser
+    grass: '#00cc66', // Saftiges Grün für Pflanze
+    electric: '#ffea00', // Knalliges Neon-Gelb für Elektro
+    psychic: '#ff3399', // Vibrantes Pink für Psycho
+    ice: '#66e6ff', // Kühles Cyan für Eis
+    dragon: '#7b4dff', // Mystisches Violett für Drache
+    normal: '#d9d9d9', // Helles, cleanes Grau für Normal
+    fighting: '#ff8c1a', // Energetisches Orange für Kampf
+    flying: '#99ccff', // Leichtes Himmelblau für Flug
+    poison: '#b833ff', // Giftiges Neon-Lila für Gift
+    ground: '#e6b800', // Warmes Goldbraun für Boden
+    rock: '#8c6b33', // Robuster Bronzeton für Gestein
+    bug: '#aaff33', // Frisches Limettengrün für Käfer
+    ghost: '#6b33cc', // Tiefes, mysteriöses Indigo für Geist
+    steel: '#b3b3cc', // Schimmerndes Silbergrau für Stahl
+    dark: '#4d4d4d', // Dunkles, edles Anthrazit für Unlicht
+    fairy: '#ff99e6', // Zuckersüßes Rosa für Fee
+  };
+
   ngOnInit() {
     if (this.pokemon()) {
       console.log('Pokemon data received:', this.pokemon());
@@ -52,5 +73,31 @@ export class SmallcardComponent {
     } else {
       return undefined;
     }
+  }
+
+  setBackgroundColor(types: any[] | undefined): string | undefined {
+    if (!types) {
+      console.warn('No types found for this Pokemon');
+      return '#f0f0f0';
+    }
+
+    const primaryType = types[0].type.name.toLowerCase();
+    const primaryColor = this.BACKGROUND_COLORS[primaryType] || '#f0f0f0';
+
+    if (types.length === 1) {
+      if (!this.BACKGROUND_COLORS[primaryType]) {
+        console.warn(`No background color defined for type: ${primaryType}`);
+      }
+      return primaryColor;
+    }
+
+    const secondaryType = types[1].type.name.toLowerCase();
+    const secondaryColor = this.BACKGROUND_COLORS[secondaryType] || '#f0f0f0';
+
+    if (!this.BACKGROUND_COLORS[secondaryType]) {
+      console.warn(`No background color defined for type: ${secondaryType}`);
+    }
+
+    return `linear-gradient(45deg, ${primaryColor}, ${secondaryColor})`;
   }
 }
--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { EndpointserviceService } from '../services/endpointservice.service';
-import { HeaderComponent } from "../shared/header/header.component";
+import { HeaderComponent } from '../shared/header/header.component';
 import { CommonModule } from '@angular/common';
 import { Pokemon } from '../interfaces/pokemon';
 import { SmallcardComponent } from '../cards/smallcard/smallcard.component';
@@ -10,19 +10,16 @@ import { SmallcardComponent } from '../cards/smallcard/smallcard.component';
   selector: 'app-landingpage',
   imports: [MatIconModule, HeaderComponent, CommonModule, SmallcardComponent],
   templateUrl: './landingpage.component.html',
-  styleUrl: './landingpage.component.scss'
+  styleUrl: './landingpage.component.scss',
 })
 export class LandingpageComponent {
-
   pokemonList: Pokemon[] = [];
   pokemonDetails: Pokemon[] = [];
 
-  constructor(public endpoint: EndpointserviceService) { 
-    
-  }
+  constructor(public endpoint: EndpointserviceService) {}
 
   ngOnInit() {
-    this.endpoint.returnPokemonList(12, 6).subscribe({
+    this.endpoint.returnPokemonList(1, 5).subscribe({
       next: (data: any) => {
         this.pokemonList = data.results;
         console.log('Pokemon list fetched:', this.pokemonList);
@@ -30,26 +27,24 @@ export class LandingpageComponent {
       error: (err: any) => {
         console.error('Error fetching Pokemon list:', err);
       },
-      complete: () => { console.log('Pokemon list fetch complete')
-      this.fetchPokemonByURL()  
-      ; }
-    })
+      complete: () => {
+        console.log('Pokemon list fetch complete');
+        this.fetchPokemonByURL();
+      },
+    });
   }
 
   fetchPokemonByURL() {
-    this.pokemonList.forEach(pkmn => {
+    this.pokemonList.forEach((pkmn) => {
       if (pkmn.url) {
         this.endpoint.returnPokemonByURL(pkmn.url).subscribe({
           next: (data: Pokemon) => {
             this.pokemonDetails.push(data);
             console.log('Pokemon details fetched:', data);
-          }
-        })
+          },
+        });
       }
     });
-    console.warn('!!!!!!!!!!!!All Pokemon details fetched:', this.pokemonDetails);
-    console.warn('!!!!!!!!!!!!All Pokemon details fetched:', this.pokemonDetails[0].sprites?.front_default);
-    console.warn('!!!!!!!!!!!!All Pokemon details fetched:', this.pokemonDetails[0].height);
   }
 
 }
